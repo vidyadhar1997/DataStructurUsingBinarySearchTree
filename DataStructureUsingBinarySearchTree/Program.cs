3d35af825f9b51f50f4625a39937c13310f212fe
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-18 13:08:36 +0530

    [vidyadhar hudge]Add:Welcome Message

---
+++ DataStructureUsingBinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureUsingBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Binary Search Tree Program");
+        }
+    }
+}
